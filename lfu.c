0e5dfb62ed294dbfbe5eb97b71895a0a765ac9f4
nandu10l <118618712+nandu10l@users.noreply.github.com>
2024-07-09 12:23:12 +0530

    Update lfu.c

--- lfu.c
+++ lfu.c
@@ -1,39 +1,39 @@
 #include<stdio.h>
 void main()
 {
-  int rs[50], i, j, k, m, f, cntr[20], a[20], min, pf=0;
+  int pages[50], i, j, k, no_page, no_frame, cntr[20], frames[20], min, pf=0;
   printf("\nEnter number of page references -- ");
-  scanf("%d",&m);
+  scanf("%d",&no_page);
   printf("\nEnter the reference string -- ");
-  for(i=0;i<m;i++)
-  scanf("%d",&rs[i]);
+  for(i=0;i<no_page;i++)
+  scanf("%d",&pages[i]);
   printf("\nEnter the available no. of frames -- ");
-  scanf("%d",&f);
-   for(i=0;i<f;i++)
+  scanf("%d",&no_frame);
+   for(i=0;i<no_frame;i++)
    {
-     cntr[i]=0; a[i]=-1;
+     cntr[i]=0; frames[i]=-1;
    }
   printf("\nThe Page Replacement Process is- \n");
-  for(i=0;i<m;i++)
+  for(i=0;i<no_page;i++)
   {
  
-    for(j=0;j<f;j++)
-      if(rs[i]==a[j])
+    for(j=0;j<no_frame;j++)
+      if(pages[i]==frames[j])
       {
         cntr[j]++;
         break;
 	}
-       if(j==f)
+       if(j==no_frame)
        {     min = 0;    
-         for(k=1;k<f;k++)
+         for(k=1;k<no_frame;k++)
              if(cntr[k]<cntr[min])
              min=k;
-             a[min]=rs[i]; cntr[min]=1;
+             frames[min]=pages[i]; cntr[min]=1;
              pf++;
        }
          printf("\n");
-         for(j=0;j<f;j++)
-         printf("\t%d",a[j]);
+         for(j=0;j<no_frame;j++)
+         printf("\t%d",frames[j]);
          
      }
      printf("\n\nTotal number of page faults -- %d",pf);
